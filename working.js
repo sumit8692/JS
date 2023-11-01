f17115b08d6bad674033efdcfaa32829566e74b7
sumit8692 <sumit8962@gmail.com>
2023-11-02 04:13:42 +0530

    updated

--- working.js
+++ working.js
@@ -1,17 +1,55 @@
-var userName = "Tom";
-var userAge = "10";
+//Execution Context
 
-console.log(`username: ${userName}`);
-console.log(`userAge:${userAge}`);
 
-function greetUser(name){
-    var greet = 'I hope you are doing fine.';
-    console.log(`Hello ${name}, ${greet}`);
-    var currentYear = 2030;
-    const year = currentYear-userAge;
+// var userName = "Tom";
+// var userAge = "10";
 
-    return `Your birth year is ${year}`;gi
-    }
+// console.log(`username: ${userName}`);
+// console.log(`userAge:${userAge}`);
 
-const birthYear = greetUser(userName);
-console.log(birthYear);
+// function greetUser(name){
+//     var greet = 'I hope you are doing fine.';
+//     console.log(`Hello ${name}, ${greet}`);
+//     var currentYear = 2030;
+//     const year = currentYear-userAge;
+
+//     return `Your birth year is ${year}`;gi
+//     }
+
+// const birthYear = greetUser(userName);
+// console.log(birthYear);
+
+
+
+// Hoisting
+
+
+// console.log(userName);
+
+// var userName = "sumit"; 
+
+
+
+// Temporal Dead Zone
+
+// var a = 10;
+// {
+//     console.log(a);
+// }
+
+// console.log(a) //will give error as a is not defined
+
+
+// console.log(a);
+// let a = 10;         // memory is allocated but it's not accessible this is temporal deadzone here let is hoisted but Javascript is not allowing to access
+
+// let a = 10;
+// {
+//     console.log(a); //output would be 10 here
+// }
+
+let a = 10;
+{
+    console.log(a);
+    let a = 10;             //javascript will show error as cannot access a variable showing as inaccessible
+}
